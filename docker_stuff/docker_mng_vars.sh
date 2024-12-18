ea604f3c8d44cff2fa305b5451e8fe052278b1cb
Ettore Landini <ettore.landini@iit.it>
2024-12-18 14:06:30 +0100

    New test
    Signed-off-by: Ettore Landini <ettore.landini@iit.it>

--- docker_stuff/docker_mng_vars.sh
+++ docker_stuff/docker_mng_vars.sh
@@ -1,6 +1,6 @@
-UBUNTU_DEF="ubuntu:22.04"
+UBUNTU_DEF="ubuntu:24.04"
 CUDA_DEF="nvidia/cuda:11.7.1-cudnn8-devel-ubuntu22.04"
-ROS_DEF="iron"
+ROS_DEF="jazzy"
 YARP_DEF="master"
 CUDA_SUFFIX="cuda.11.7.1-cudnn8"
 UBUNTU_SUFFIX="ubuntu22.04"
